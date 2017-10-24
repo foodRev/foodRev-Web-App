d63e4caf73a4d16bad946734667589e400551efd
greg@foodrev.org <greg@foodrev.org>
2017-10-24 00:47:24 -0700

    rename files, remove extra files, remove routes

--- main.py
+++ main.py
@@ -11,52 +11,9 @@ app = Flask(__name__)
 
 # [START Login]
 @app.route('/')
-def login():
-    return render_template('login_content.html')
+def content():
+    return render_template('content.html')
 # [END Login]
-
-# [START Login Success Landing]
-@app.route('/login_success', methods=['POST'])
-def login_success():
-    name = request.form['name']
-    email = request.form['email']
-    return render_template(
-        'login_success_landing.html',
-        name=name,
-        email=email)
-# [END Login Success Landing]
-
-# [START auth]
-@app.route('/auth')
-def auth():
-    return render_template('firebase_auth.html')
-# [END auth]
-
-# [START form]
-@app.route('/form')
-def form():
-    return render_template('form.html')
-# [END form]
-
-
-# [START submitted]
-@app.route('/submitted', methods=['POST'])
-def submitted_form():
-    name = request.form['name']
-    email = request.form['email']
-    site = request.form['site_url']
-    comments = request.form['comments']
-
-    # [END submitted]
-    # [START render_template]
-    return render_template(
-        'submitted_form.html',
-        name=name,
-        email=email,
-        site=site,
-        comments=comments)
-    # [END render_template]
-
 
 @app.errorhandler(500)
 def server_error(e):
